c1f88a0ed1cbe435011be9be8c66bfe9d0054290
Manikandan <89195312+manikandan531@users.noreply.github.com>
2022-12-14 19:14:53 +0530

    Update CustomUserDetailService.java

--- E-Commerce/src/main/java/com/example/demo/api/service/CustomUserDetailService.java
+++ E-Commerce/src/main/java/com/example/demo/api/service/CustomUserDetailService.java
@@ -1,36 +1,80 @@
 package com.example.demo.api.service;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.api.entity.Customer;
 import com.example.demo.api.repository.CustomerRepo;
+import com.example.demo.api.repository.ProductRepo;
 
 @Service
-public class CustomUserDetailService  implements UserDetailsService
+public abstract class CustomerServiceImple  implements CustomerService
 {
 	@Autowired
-	CustomerRepo customerRepo;
+	private CustomerRepo cusrepo;
+	
+	@Autowired
+	private ProductRepo prorepo;
 
 	@Override
-	public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException 
+	public Customer saveCustomer(Customer customer)
 	{
-		Customer customer = customerRepo.findByName(name);
-		if(customer==null)
-		{
-			throw new UsernameNotFoundException("invalidusername");
-		}
-		return new User(customer.getName(),customer.getPassword(),new ArrayList<>());
+		return cusrepo.save(customer);
 	}
 
-	
-	
-	
+	@Override
+	public List<Customer> showCustomer(Customer customer) 
+	{
+		
+		return cusrepo.findAll();
+	}
 
+	@Override
+	public void deleteCustomer(Integer id) 
+	{
+		
+		cusrepo.deleteById(id);
+	}
+
+	@Override
+	public Customer updateCustomer(Integer id, Customer customer)
+	{
+		
+		        Customer updatedvalue=cusrepo.findById(id).get();
+		        System.out.println("name"+customer.getUsername());
+		        System.out.println("emil"+customer.getEmail());
+		        System.out.println("password"+customer.getPassword());
+		        System.out.println("phone"+customer.getPhone());
+		        
+		        if(Objects.nonNull(customer.getUsername()) && !"".equalsIgnoreCase(customer.getUsername()))
+		        {
+		            updatedvalue.setUsername(customer.getUsername());
+		            
+		        }
+		        
+		        if(Objects.nonNull(customer.getEmail()) && !"".equalsIgnoreCase(customer.getEmail()))
+		        {
+		            updatedvalue.setEmail(customer.getEmail());
+		        }
+		        
+
+		        if(Objects.nonNull(customer.getPassword()) && !"".equalsIgnoreCase(customer.getPassword()))
+		        {
+		            updatedvalue.setPassword(customer.getPassword());
+		        }
+		        
+		        
+		        if(Objects.nonNull(customer.getPhone()) && !"".equalsIgnoreCase(customer.getPhone()))
+		        {
+		            updatedvalue.setPhone(customer.getPhone());
+		        }
+		        return cusrepo.save(updatedvalue);
+		    
+	}
+	
+	
+	
 }
